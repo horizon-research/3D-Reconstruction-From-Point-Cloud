2cd52512d809fb3bf7f0ad7b57b3f69d7c6e0e58
sye8 <sye8@u.rochester.edu>
2020-08-08 00:11:46 -0400

    Added set for neighboring points of tri

--- C++/pointsTransfer.cpp
+++ C++/pointsTransfer.cpp
@@ -48,7 +48,6 @@ int main(int argc, char** argv){
     const unsigned int K = 20; // Search range
 
     CGAL::Timer task_timer; task_timer.start();
-//    CGAL::Real_timer real_task_timer; real_task_timer.start();
     CGAL::Real_timer real_total_timer; real_total_timer.start();
 
     // Read original point set
@@ -95,7 +94,7 @@ int main(int argc, char** argv){
         }
     }
 
-    std::cout << "Point count: " << point_count << std::endl;
+    std::cout << "PC Point count: " << point_count << std::endl;
 
     int pos = 0;
 	int counter = 0;
@@ -172,14 +171,13 @@ int main(int argc, char** argv){
 		}   
 	}
 
-    std::cout << "Read point set cost: " << task_timer.time() << " seconds" << std::endl;
+    std::cout << "Read point set in: " << task_timer.time() << " seconds" << std::endl;
     task_timer.reset();
 
     // Insert number_of_data_points in the tree
     Tree tree(points.begin(), points.end());
-    Distance tr_dist;
 
-    std::cout << "Build Kd tree cost: " << task_timer.time() << " seconds" << std::endl;
+    std::cout << "Built Kd tree in: " << task_timer.time() << " seconds" << std::endl;
     task_timer.reset();
 
     pc_file_stream.close();
@@ -232,8 +230,8 @@ int main(int argc, char** argv){
 		}
 	} 
 
-    std::cout << "Vertex count: " << vertex_count << std::endl;
-    std::cout << "Face count: " << face_count << std::endl;
+    std::cout << "Mesh vertex count: " << vertex_count << std::endl;
+    std::cout << "Mesh face count: " << face_count << std::endl;
 
     pos = 0;
 	counter = 0;
@@ -319,29 +317,9 @@ int main(int argc, char** argv){
 		}   
 	}
 
-    // boost::property_tree::ptree pt, output, child1, ans;
-    // child1.put("sd", "sdf");
-    // pt.push_back(std::make_pair("", child1));
-    // pt.push_back(std::make_pair("", child1));
-    // pt.push_back(std::make_pair("", child1));
-    // pt.push_back(std::make_pair("", child1));
-    // for(int index = 0; index < 2; index++){
-    //   output.push_back(std::make_pair("", pt));
-    // }
-    // ans.add_child("res", output);
-    // child1.put("sd", "---");
-
-    // std::stringstream ss;
-    // boost::property_tree::json_parser::write_json(ss, ans);
-    // std::cout << ss.str() << std::endl;
-
-    boost::property_tree::ptree pt, triangle, pts, ptList, single, data, output;
-
-    int ver[4];
-    int i = 1;
-    Point v1, v2, v3;
     counter = 0;
     pos = 0;
+    Point triangle[3];
 	// Read faces
 	while(!mesh_file_stream.eof())
 	{
@@ -362,68 +340,69 @@ int main(int argc, char** argv){
             }
             else if(pos % 4 == 0)
 			{
-				ver[0] = atof(str.c_str());              
+                // Ignore polygon vertex count
+                // Assume all polygons are triangles
 				str.clear();
 			}
 			else if(pos % 4 == 1)
 			{
-				ver[1] = atof(str.c_str());                    
-				str.clear ( );      
+				triangle[0] = vertices[atof(str.c_str())];
+				str.clear();
 			}
 			else if(pos % 4 == 2)
 			{
-				ver[2] = atof(str.c_str());                   
+				triangle[1] = vertices[atof(str.c_str())];
 				str.clear();
 			}
             else if(pos % 4 == 3)
 			{
-				ver[3] = atof(str.c_str());                   
+				triangle[2] = vertices[atof(str.c_str())];
 				str.clear();
-            
-                counter++;
-        
-                v1 = vertices[ver[1]];
-                v2 = vertices[ver[2]];
-                v3 = vertices[ver[3]];
                 
-                for(int index = 1; index <= 3; index++){
-                    // Store the vertices of the triangle
-                    pt.put("v", vertices[ver[index]].detailWithUV());
-                    triangle.push_back(std::make_pair("", pt));
+                counter++;
 
-                    // Look for points near the vertices
-                    K_neighbor_search search(tree, vertices[ver[index]], K);
-                    for(K_neighbor_search::iterator it = search.begin(); it != search.end(); it++){
-                        pts.put("p", (it->first).detail());
-                        ptList.push_back(std::make_pair("", pts));
-                    }
+                // Print triangle
+                std::cout << "Triangle:" << std::endl;
+                for(int i = 0; i < 3; i++)
+                {
+                    std::cout << triangle[i].detail() << std::endl;
                 }
                 
-                single.add_child("triangle", triangle);
-                triangle.clear();
-                single.add_child("points", ptList);
-                ptList.clear();
-                data.push_back(std::make_pair("",single));
-                single.clear();
+                // Print found neighbors
+                std::cout << "Neighbors found:" << std::endl;
+                
+                // Find nearest points to the triangle
+                std::set<Point, point_set_comparator> neighbors;
+                for(int i = 0; i < 3; i++)
+                {
+                    K_neighbor_search search(tree, triangle[i], K);
+                    for(K_neighbor_search::iterator it = search.begin(); it != search.end(); it++)
+                    {
+                        neighbors.insert(it->first);
+                    }
+                    std::cout << std::endl;
+                }
+                
+                // Find rotation of triangle to up around centroid
+                
+                // For each neighboring point
+                for(auto it = neighbors.begin(); it != neighbors.end(); it++)
+                {
+                    
+                }
+                
             }
             pos++;
         }
 	}
     mesh_file_stream.close();
-    
-    output.add_child("data", data);
 
-    std::cout << "Search near points cost: " << task_timer.time() << " seconds" << std::endl;
+    std::cout << "Nearest neighbors search total time: " << task_timer.time() << " seconds" << std::endl;
     task_timer.reset();
     
-    std::stringstream ss;
-    boost::property_tree::json_parser::write_json(ss, output);
-    std::ofstream outFile;
-    outFile.open("data/pointsMesh.json");
-    outFile << ss.str();
-    outFile.close();
-
-    std::cout << "Writing output cost: " << task_timer.time() << " seconds" << std::endl;
+    // Output
+    
+    std::cout << "Output time: " << task_timer.time() << " seconds" << std::endl;
     task_timer.reset();
 
     std::cout << "Total real time: " << real_total_timer.time() << " seconds" << std::endl;
@@ -431,22 +410,6 @@ int main(int argc, char** argv){
     
     std::cout << "VIRT: " << (CGAL::Memory_sizer().virtual_size() >> 20)  << " MiB" << std::endl;
     std::cout << "RES:  " << (CGAL::Memory_sizer().resident_size() >> 20) << " MiB" << std::endl;
-
-//    // search K nearest neighbours
-//    K_neighbor_search search(tree, vertices[0], K);
-//    for(K_neighbor_search::iterator it = search.begin(); it != search.end(); it++){
-//        std::cout << " d(q, nearest neighbor)=  " << (it->first).x()
-//                  << tr_dist.inverse_of_transformed_distance(it->second) << std::endl;
-//    }
-
-//    // Display points read
-//    for (std::size_t i = 0; i < 3; ++ i)
-//    {
-//      const double x = points[i].x();
-//      const double y = points[i].y();
-//      const double z = points[i].z();
-//      std::cerr << "Point (" << x << "  " << y << " "<< z << ") "<< std::endl;
-//    }
 
     return 0;
 }
